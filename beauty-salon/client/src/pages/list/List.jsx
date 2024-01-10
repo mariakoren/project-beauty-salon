d6f014376338a41106c51595a0c96fb5cdea19f2
maria_koren <maria_koren@yahoo.com>
2024-01-10 20:28:43 +0100

    service page, search item

--- beauty-salon/client/src/pages/list/List.jsx
+++ beauty-salon/client/src/pages/list/List.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {useLocation} from  'react-router-dom';
+import './list.css';
+import NavBar from '../../components/navbar/navbar.jsx';
+import {format} from "date-fns";
+import {DateRange} from 'react-date-range';
+
 
 const List = () => {
+    const location = useLocation();
+    const [service, setService] = useState(location.state.service);
+    const [date, setDate] = useState(location.state.date);
+    const [openDate, setOpenDate] = useState(false);
+
+
+
     return (
-        <div>List</div>
+        <div>
+            <NavBar/>
+            <div className="listContainer">
+                <div className="listWrapper">
+                    <div className="listSearch">
+                        <h1 className="lsTitle">Search</h1>
+                        <div className="lsItem">
+                            <label>Nazwa zabiegu</label>
+                            <input type="text" placeholder={service} />
+                        </div>
+                        <div className="lsItem">
+                            <label>Interesujące dni</label>
+                            <span onClick={()=> setOpenDate(!openDate)}>{`${format(date[0].startDate, 'MM/dd/yyyy')} do ${format(date[0].endDate, 'MM/dd/yyyy')}  `}</span>
+                            {openDate && <DateRange 
+                            onChange={item=>setDate(item.selection)}
+                            minDate={new Date()}
+                            ranges={date}
+                            />}
+                        </div>
+                       
+                        
+            
+                    </div>
+                    <div className="listResult"></div>
+                </div>
+            </div>
+
+        </div>
+
     )
+        
 }
 
 export default List;
