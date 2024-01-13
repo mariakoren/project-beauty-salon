1876b25f40ae2d2216b2adf563ae3f60bf8914eb
maria_koren <maria_koren@yahoo.com>
2024-01-13 17:27:51 +0100

    first fetching brom db

--- beauty-salon/api/models/service.js
+++ beauty-salon/api/models/service.js
@@ -5,6 +5,10 @@ const ServiceSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    type: {
+        type: String, 
+        required: true
+    },
     desc: {
         type: String,
         required: true
