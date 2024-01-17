bd9d407d5772557f36079d188580d7adf340f938
maria_koren <maria_koren@yahoo.com>
2024-01-17 17:04:23 +0100

    reservations for each client, average price

--- beauty-salon/api/controllers/reservation.js
+++ beauty-salon/api/controllers/reservation.js
@@ -1,13 +1,15 @@
 import Reservation from "../models/reservstion.js";
 import User from "../models/user.js";
+import Service from "../models/service.js";
 
-export const createReservation = async (req, res) => {
+export const createReservation = async (req, res, next) => {
     const newReservation = new Reservation(req.body);
     try {
         const savedReservation = await newReservation.save();
         res.status(200).json(savedReservation)
     } catch(err){
-        res.status(500).json(err);
+        // res.status(500).json(err);
+        next(err)
     }
 }
 
@@ -44,19 +46,16 @@ export const deleteReservation = async (req, res) => {
     }
 }
 
-export const getReservation = async (req, res) => {
-
+export const getReservationForPerson = async (req, res) => {
+    const userIdParam = req.query.id;
     try {
-        const reservation = await Reservation.findById(
-            req.params.id, 
-            )
-
-        res.status(200).json(reservation)
-    } catch(err){
-        res.status(500).json(err);
+        const reservations = await Reservation.find({ userId: userIdParam });
+        res.status(200).json(reservations);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Błąd podczas przetwarzania zapytania.' });
     }
-} 
-
+};
 export const getReservations = async (req, res) => {
     try {
         const reservations = await Reservation.find(req.query)
@@ -65,3 +64,27 @@ export const getReservations = async (req, res) => {
         res.status(500).json(err);
     }
 }
+
+export const getAveragePriceForUser = async (req, res) => {
+    const userIdParam = req.query.id;
+    try {
+        const reservations = await Reservation.find({ userId: userIdParam });
+        if (reservations.length === 0) {
+            return res.status(404).json({ message: 'Brak rezerwacji dla podanego użytkownika.' });
+        }
+        let totalPrices = 0;
+        for (const reservation of reservations) {
+            const serviceId = reservation.serviceId;
+            const service = await Service.findOne({ _id: serviceId });
+            if (service) {
+                totalPrices += service.price;
+            }
+        }
+        const averagePrice = totalPrices / reservations.length;
+        res.status(200).json({ averagePrice });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Błąd podczas przetwarzania zapytania.' });
+    }
+}
+
