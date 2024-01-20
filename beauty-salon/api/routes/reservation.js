41907fa0ec05c2d91df11868387aa324a692e865
maria_koren <maria_koren@yahoo.com>
2024-01-20 22:50:25 +0100

    fix: reservation exceptions

--- beauty-salon/api/routes/reservation.js
+++ beauty-salon/api/routes/reservation.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createReservation, getAveragePriceForUser, getReservationForPerson, getReservations, confirmedReservation, deleteReservation } from "../controllers/reservation.js";
+import { createReservation, getAveragePriceForUser, getReservationForPerson, getReservations, confirmedReservation, deleteReservation, deleteReservationByAdmin } from "../controllers/reservation.js";
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.post("/", createReservation)
 //DELETE
 // router.delete("/:id",verifyUser, deleteReservation)
 router.delete("/:id",  deleteReservation)
+router.delete("/admin/:id", verifyAdmin, deleteReservationByAdmin)
 //GET
 router.get("/find", getReservationForPerson)
 router.get("/average", verifyAdmin, getAveragePriceForUser)
